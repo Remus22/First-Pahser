c903284fa2d203b6f34b73ba4b53d8c23d91c81f
Ramius Chun <rchun.sh@mysummitps.org>
2016-11-10 19:43:21 +0000

    update

---
+++ myGame/Story.js
@@ -0,0 +1,36 @@
+/* global game phaser game_state*/
+game_state.story = function(){};
+
+game_state.story.prototype = {
+    
+
+    preload: function() {
+        game.load.image('sky', 'assets/sky.png');
+        game.load.image('ground', 'assets/platform.png');
+        game.load.image('star', 'assets/Sapphire.png');
+        game.load.spritesheet('Black Mage', 'assets/Black Mage.png', 192, 192 );
+    },
+
+    create: function() {
+        
+        this.storyText = game.add.text(200, game.world.height / 2, 'Welcome to the game./n Press enter to start)',{
+            fill: '#ffffff'
+            
+        });
+     this.cursors = game.input.keyboard.createCursorKeys();
+        this.space.inputEnabled = true;
+        var gameNum = 1;
+    },
+    
+    
+    update: function() {
+        if(this.cursor.down.isDown){ 
+            
+            game.state.start('main')
+            
+        }
+
+},
+
+game.state.add('story', game_state.story);
+game.state.add('story' game_state.sto)
--- myGame/myGame.js
+++ myGame/myGame.js
@@ -12,8 +12,8 @@ game_state.main.prototype = {
     preload: function() {
         game.load.image('sky', 'assets/sky.png');
         game.load.image('ground', 'assets/platform.png');
-        game.load.image('star', 'assets/star.png');
-        game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
+        game.load.image('star', 'assets/Sapphire.png');
+        game.load.spritesheet('Black Mage', 'assets/Black Mage.png', 192, 192 );
 
     },
 
@@ -26,29 +26,35 @@ game_state.main.prototype = {
         this.platforms = game.add.group();
         this.platforms.enableBody = true;
         var ground = this.platforms.create(0, game.world.height - 64, 'ground');
+        
         ground.scale.setTo(2, 2);
         ground.body.immovable = true;
         
         var ledge = this.platforms.create(200, 400, 'ground');
         ledge.body.immovable = true;
+        var ledge= this.platforms.create(800, 600, 'ground');
+        ledge.body.immovable = true;
         
         
-        this.player = game.add.sprite(32, game.world.height - 150, 'dude');
+          
+        this.player = game.add.sprite(32, game.world.height - 180, 'Black Mage');
         game.physics.arcade.enable(this.player);
-        this.player.body.bounce.y = 1 - 0;
-        this.player.body.gravity.y = 100 - 50;
+        this.player.body.bounce.y = 1 - 0.8;
+        this.player.body.gravity.y = 400 - 50;
         this.player.body.collideWorldBounds = true;
-        this.player.animations.add('left', [0, 1, 2, 3], 10, true);     
-        this.player.animations.add('right', [5, 6, 7, 8], 10, true);
+        this.player.animations.add('left', [1, 2], 10, true);     
+        this.player.animations.add('right', [3, 4], 10, true);
+        this.player.scale.setTo(0.5,0.5);
+            
         
         this.cursors = game.input.keyboard.createCursorKeys();
         
         this.stars = game.add.group();
         this.stars.enableBody = true;
-        for (var i = 0; i < 12; i++) {
+        for (var i = 0; i < 12; i++) {  
             var star = this.stars.create(i * 70, 0, 'star');
             star.body.gravity.y = 300;
-            star.body.bounce.y = 0.7 + Math.random() * 0.2;
+            star.body.bounce.y = 0.6 + Math.random() * 0.2;
 
         }
         this.scoreText = game.add.text(16, 16, 'score: 0', {
@@ -77,22 +83,27 @@ game_state.main.prototype = {
         }
         else {
             this.player.animations.stop();
-            this.player.frame = 4;
+            this.player.frame = 0;
         }
 
         if (this.cursors.up.isDown && this.player.body.touching.down) {
             this.player.body.velocity.y = -350;
         }
-        
-        
+    
+     this.player.body.setSize(170, 170, 2,1);
+     
 
     },
     collectStar: function(player, star) {
         this.score++;
         this.scoreText.text = "Score: " + this.score;
-        
         star.kill();
+           var star = this.stars.create( Math.random() * 800, Math.random() * 600, 'star');
+            star.body.gravity.y = 300;
+            star.body.bounce.y = .5 + Math.random() * .5;
+            ;
     }
 };
 game.state.add('main', game_state.main);
-game.state.start('main');
+
+
